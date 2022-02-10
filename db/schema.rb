43c51dcd7ed686aad09d12f74f89a65b26aebfeb
EC2 Default User <ec2-user@ip-172-31-23-176.ec2.internal>
2022-02-10 16:08:44 +0000

    customer signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_120447) do
+ActiveRecord::Schema.define(version: 2022_02_10_152311) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 2022_02_09_120447) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "first_name_kana"
+    t.string "last_name_kana"
+    t.string "postal_code"
+    t.string "address"
+    t.string "telephone_number"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
